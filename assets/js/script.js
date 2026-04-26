cb1100510d12d06fdf752d3e40cf7ee2f33da07c
Sebastián Jaramillo <sebajaramilloy@gmail.com>
2026-04-26 16:48:07 -0300

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-[span_0](start_span)// Arreglo inicial con al menos 3 tareas[span_0](end_span)
+// Arreglo inicial con al menos 3 tareas
 const tareas = [
     { id: 16, descripcion: "Hacer mercado", completado: false },
     { id: 60, descripcion: "Estudiar para la prueba", completado: false },
@@ -11,7 +11,7 @@ const listaTareas = document.querySelector("#listaTareas");
 const totalTareas = document.querySelector("#totalTareas");
 const tareasRealizadas = document.querySelector("#tareasRealizadas");
 
-[span_1](start_span)// Función para mostrar las tareas en el HTML[span_1](end_span)
+// Función para mostrar las tareas en el HTML
 function renderizar() {
     let html = "";
     
@@ -37,7 +37,7 @@ function renderizar() {
     actualizarResumen(); 
 }
 
-[span_2](start_span)[span_3](start_span)// Agregar nueva tarea al arreglo[span_2](end_span)[span_3](end_span)
+// Agregar nueva tarea al arreglo
 btnAgregar.addEventListener("click", () => {
     const nuevaDescripcion = inputTarea.value;
 
@@ -47,9 +47,9 @@ btnAgregar.addEventListener("click", () => {
     }
 
     const nuevaTarea = {
-        [span_4](start_span)id: Date.now(), // ID único basado en tiempo[span_4](end_span)
+        id: Date.now(), // ID único basado en tiempo
         descripcion: nuevaDescripcion,
-        [span_5](start_span)completado: false // Iniciar con completado: false[span_5](end_span)
+        completado: false // Iniciar con completado: false
     };
 
     tareas.push(nuevaTarea);
@@ -57,21 +57,21 @@ btnAgregar.addEventListener("click", () => {
     renderizar();
 });
 
-[span_6](start_span)[span_7](start_span)// Eliminar tarea del arreglo[span_6](end_span)[span_7](end_span)
+// Eliminar tarea del arreglo
 function borrarTarea(id) {
     const index = tareas.findIndex(ele => ele.id === id);
     tareas.splice(index, 1);
     renderizar();
 }
 
-[span_8](start_span)[span_9](start_span)// Cambiar el estado de completado[span_8](end_span)[span_9](end_span)
+// Cambiar el estado de completado
 function cambiarEstado(id) {
     const index = tareas.findIndex(ele => ele.id === id);
     tareas[index].completado = !tareas[index].completado;
     renderizar();
 }
 
-[span_10](start_span)[span_11](start_span)// Actualizar contadores de total y realizadas[span_10](end_span)[span_11](end_span)
+// Actualizar contadores de total y realizadas
 function actualizarResumen() {
     totalTareas.innerHTML = tareas.length;
     const conteoRealizadas = tareas.filter(t => t.completado).length;
